f42dbca1d09328154f3c684fd61bd13bd46a96e9
Nadeau Ludovick <2232599@cegepmontpetit.ca>
2023-02-02 14:05:14 -0500

    FCT: Ex4

--- Ex4_CreationDossiersAvecListEtCsv.py
+++ Ex4_CreationDossiersAvecListEtCsv.py
@@ -3,12 +3,7 @@ os.chdir(os.path.dirname(__file__))   # Elles permettent de se positionner dans 
 
 
 # importez les packages os et csv #
-
-
-
-
-
-
+import csv
 
 # dans ce script vous allez faire les étapes suivantes:
 #   - créez une variable qui va contenir le nom du fichier csv à lire, soit ici 'Ex4_ListeEtudiants_cours4201B3EM_gr1010.csv'
@@ -24,8 +19,19 @@ os.chdir(os.path.dirname(__file__))   # Elles permettent de se positionner dans 
 #   - Ajoutez les noms et prénoms des étudiants qui sont dans le fichier dans votre liste des étudiants
 #   - Déplacez vous dans l'arborescence des dossiers pour aller dans le dossier du groupe que vous avez créé
 #   - Finalement faites une boucle pour créer un dossier avec les noms et prénoms que vous avez dans votre liste.
-
-
-
-
+NomFichier = 'Ex4_ListeEtudiants_cours4201B3EM_gr1010.csv'
+NomFichierSplit = NomFichier.split('.')
+cours = NomFichierSplit[0][NomFichierSplit[0].find('420'):NomFichierSplit[0].find('420')+6]
+groupe = NomFichierSplit[0][NomFichierSplit[0].find('gr'):NomFichierSplit[0].find('gr')+6]
+os.makedirs(f"""{cours}/{groupe}""")
+with open('Ex4_ListeEtudiants_cours4201B3EM_gr1010.csv','r', encoding='utf-8') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    next(csv_reader)
+    cestbad = []
+    for line in csv_reader:
+        cestbad.append(line[0].split(';'))
+    os.chdir(f"""{cours}/{groupe}""")
+    for i in cestbad:
+        os.makedirs(f"""{i[2]+ ' ' + i[3]}""")
+print(groupe)
 
